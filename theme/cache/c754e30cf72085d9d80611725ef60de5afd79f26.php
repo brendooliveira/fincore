75c11e5c8f15e3bd7d9e87f53c6d0014c66cde4a
Brendo Oliveira <brendo.dev@outlook.com>
2023-04-24 08:37:33 -0300

    att

--- theme/cache/c754e30cf72085d9d80611725ef60de5afd79f26.php
+++ theme/cache/c754e30cf72085d9d80611725ef60de5afd79f26.php
@@ -2,4 +2,25 @@
     <header class="py-4">
         <h2 class="testemunhos_h2">Veja o que nossos <br> clientes dizem</h2>
     </header>
+
+    <div class="row">
+        <div class="col-lg-4">
+            <div class="card">
+                <div class="card-body">
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="card">
+                <div class="card-body">
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="card">
+                <div class="card-body">
+                </div>
+            </div>
+        </div>
+    </div>
 </section><?php /**PATH C:\laragon\www\fincore\theme\view/web/components/section-testemunhos.blade.php ENDPATH**/ ?>
--- theme/view/web/components/section-testemunhos.blade.php
+++ theme/view/web/components/section-testemunhos.blade.php
@@ -2,4 +2,25 @@
     <header class="py-4">
         <h2 class="testemunhos_h2">Veja o que nossos <br> clientes dizem</h2>
     </header>
+
+    <div class="row">
+        <div class="col-lg-4">
+            <div class="card">
+                <div class="card_body_testemunhos">
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="card">
+                <div class="card_body_testemunhos">
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="card">
+                <div class="card_body_testemunhos">
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
